54f069c1a9574c5252797bc26f6e6d95f5837ba2
Raphael Robatsch <raphael-git@tapesoftware.net>
2023-03-03 15:21:00 +0100

    Improve cutscene skip
    Replaces the cutscene skip logic with the "no cutscene patch" logic.
    This makes the cutscenes much faster. It also brings back the yes/no
    prompts, but this is a good thing due to Colosso.

--- src/Dialogue.py
+++ src/Dialogue.py
@@ -2,10 +2,8 @@ def dialogue(rom, flags):
 
     if flags['CutDialogue']:
         print('Supressing most dialogue')
-        rom.write_to_rom(0x08092F84, [0x4770]) # Text box
-        rom.write_to_rom(0x08092C40, [0x4770]) # Yes/No (?)
-        rom.write_to_rom(0x08091C7C, [0x4770]) # Yes/No (?)
-        rom.write_to_rom(0x080931ec, [0x4770]) # Multiple text boxes
+        # Cutscene skip function: jump over keypad and debug mode checks
+        rom.write_to_rom(0x080915F2, [0xE014])
     else:
         rom.write_to_rom(0x08890800, [0x4770])
         
